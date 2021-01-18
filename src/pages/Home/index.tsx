db9273a6cc45d5043431f56e12cd8e368c29fdb1
dannyelgjl <dannyelgjl@gmail.com>
2021-01-18 12:41:08 -0300

    feature/Book search form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, FormEvent } from 'react';
 // Api service
 import api from '../../services/api';
 
 
+
 const Home: React.FC = () => {
-  useEffect(() => {
-    api.get(`volumes?q=flowers&filter=free-ebooks&key=${process.env.REACT_APP_API_KEY_GOOGLE_BOOK}`).then(response => {
-      console.log(response.data.items)
-    })
-  }, []);
+  const [book, setBook] = useState("");
+  const [results, setResults] = useState([]);
+
+
+  // useEffect(() => {
+
+  // }, []);
+
+  // const handleChange = useCallback((event) => {
+  //   const book = event.targe.value;
+
+  //   setBook(book);
+  // }, []);
+
+  const handleSubmit = useCallback(async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+
+    api.get(`volumes?q=${book}&key=${process.env.REACT_APP_API_KEY_GOOGLE_BOOK}&maxResults=40`)
+      .then(response => {
+        console.log(response.data)
+      })
+
+    console.log(book);
+  }, [book]);
 
   return (
-    <h1>South System</h1>
+    <>
+      <h1>Buscar Livros</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={book}
+          onChange={e => setBook(e.target.value)}
+        />
+
+        <button type="submit">Buscar</button>
+      </form>
+
+    </>
   )
 }
 

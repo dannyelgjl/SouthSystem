f7c838d74a39e609c317e35106408da6fe5222e0
dannyelgjl <dannyelgjl@gmail.com>
2021-01-18 12:11:21 -0300

    feature/Implementing axios and consuming the google books API

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// Api service
+import api from '../../services/api';
 
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    api.get(`volumes?q=flowers&filter=free-ebooks&key=${process.env.REACT_APP_API_KEY_GOOGLE_BOOK}`).then(response => {
+      console.log(response.data.items)
+    })
+  }, []);
+
   return (
     <h1>South System</h1>
   )
